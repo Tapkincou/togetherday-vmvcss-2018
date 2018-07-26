9dda94095421d57b2aefa42d90211171a06948dd
Tapkincou <m.barreau.alexis@gmail.com>
2018-07-26 13:40:07 +0200

    Update rxjs package + Add timer countdown on the page

--- togetherdayvmvcss2018/src/app/app.component.ts
+++ togetherdayvmvcss2018/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { timer } from 'rxjs'; 
+import { take, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  countDown;
+  count = 5*60;
+
+  goTimer(){
+    console.log('goTimer');
+    this.countDown = 
+      timer(0,1000)
+        .pipe(
+        take(this.count),
+        map(()=> Math.floor(--this.count / 60) + ':' + (this.count % 60))
+        );
+  }
+
 }
