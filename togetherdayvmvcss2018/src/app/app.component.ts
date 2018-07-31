6b329580c152c30a6f10dd6600f753f24a3794ff
Tapkincou <m.barreau.alexis@gmail.com>
2018-07-31 16:38:37 +0200

    Adding angular/materials and prepare questions loop

--- togetherdayvmvcss2018/src/app/app.component.ts
+++ togetherdayvmvcss2018/src/app/app.component.ts
@@ -2,16 +2,38 @@ import { Component } from '@angular/core';
 import { timer } from 'rxjs'; 
 import { take, map } from 'rxjs/operators';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  questions = 
+  [
+    {texte: "question 1 ?",
+      reponses: [
+        {texte:"réponse A", correcte:false},
+        {texte:"réponse B", correcte:true},  
+        {texte:"réponse C", correcte:false},
+      ]
+    },
+    {texte: "question 2 ?",
+      reponses: [
+        {texte:"réponse A", correcte:true},
+        {texte:"réponse B", correcte:false},  
+        {texte:"réponse C", correcte:false},
+      ]
+    }
+  ];
   title = 'app';
   countDown;
-  count = 5*60;
+  count;
+  constructor(){
+    this.count = 10;
 
+    //called first time before the ngOnInit()
+ }
   goTimer(){
     console.log('goTimer');
     this.countDown = 
--- togetherdayvmvcss2018/src/app/app.module.ts
+++ togetherdayvmvcss2018/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCheckboxModule} from '@angular/material';
 import { AppComponent } from './app.component';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
