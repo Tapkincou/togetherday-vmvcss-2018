fdc236b5acc070037ab0e331b1e9ba27886848b0
Tapkincou <m.barreau.alexis@gmail.com>
2018-09-10 17:33:31 +0200

    [ADD]  preparation pour le coffre

--- togetherdayvmvcss2018/src/app/app.component.ts
+++ togetherdayvmvcss2018/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { timer, Observable, BehaviorSubject } from 'rxjs';
+import { timer, Observable, BehaviorSubject } from 'rxjs'; 
 import { take, map } from 'rxjs/operators';
 import { stringify } from 'querystring';
 
@@ -9,63 +9,62 @@ import { stringify } from 'querystring';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  questions =
+  questions = 
   [
-    {texte: 'Vous êtes bloqué sur le projet, que faites-vous ?',
+    {texte: "Vous êtes bloqué sur le projet, que faites-vous ?",
       reponses: [
-        {id: 0, texte: 'Je cherche tout seul jusqu\'à ce que je trouve', correcte: false, icone: 'coffee'},
-        {id: 1, texte: 'Je demande de l\'aide à mes co-équipiers puis j\'alerte si nécessaire', correcte: true, icone: 'umbrella'},
-        {id: 2, texte: 'Je demande conseille au client', correcte: false, icone: 'wrench'},
+        {id:0,texte:"Je cherche tout seul jusqu'à ce que je trouve", correcte:false, icone:"coffee"},
+        {id:1,texte:"Je demande de l'aide à mes co-équipiers puis j'alerte si nécessaire", correcte:true, icone:"umbrella"},  
+        {id:2,texte:"Je demande conseille au client", correcte:false, icone:"wrench"},
       ]
     },
-    {texte: 'Les spécifications sont incomplètes.. ou ambiguës',
+    {texte: "Les spécifications sont incomplètes.. ou ambiguës",
       reponses: [
-        {id: 3, texte: 'Vous demandez au Business Analyst', correcte: true},
-        {id: 4, texte: 'Vous passez à une autre exigence mieux spécifiée pour ne pas être bloqué', correcte: false},
-        {id: 5 , texte: 'Je demande conseille au client', correcte: false},
+        {id:3,texte:"Vous demandez au Business Analyst", correcte:true},
+        {id:4,texte:"Vous passez à une autre exigence mieux spécifiée pour ne pas être bloqué", correcte:false},  
+        {id:5,texte:"Je demande conseille au client", correcte:false},
       ]
     },
-    {texte: 'Votre code compile',
+    {texte: "Votre code compile",
       reponses: [
-        {id: 6, texte: 'Vous le mettez dans le Repository et faite un COMMIT des sources', correcte: false},
-        {id: 7, texte: 'Je fais mes tests unitaires, puis je synchronise mon environnement,'
-                        + 'avant de faire un COMMIT de mes sources', correcte: true},
-        {id: 8, texte: 'Je passe au codage de l\'exigence suivante', correcte: false},
+        {id:6,texte:"Vous le mettez dans le Repository et faite un COMMIT des sources", correcte:false},
+        {id:7,texte:"Je fais mes tests unitaires, puis je synchronise mon environnement, avant de faire un COMMIT de mes sources", correcte:true},  
+        {id:8,texte:"Je passe au codage de l'exigence suivante", correcte:false},
       ]
     },
-    {texte: 'L\'intégration continue est plantée ! Que faire ?',
+    {texte: "L'intégration continue est plantée ! Que faire ?",
       reponses: [
-        {id: 9, texte: 'Consulter le rapport de bugs', correcte: true},
-        {id: 10, texte: 'Alerter le client', correcte: false},
-        {id: 11, texte: 'Chercher dans le code', correcte: false},
+        {id:9,texte:"Consulter le rapport de bugs", correcte:true},
+        {id:10,texte:"Alerter le client", correcte:false},  
+        {id:11,texte:"Chercher dans le code", correcte:false},
       ]
     },
-    {texte: 'Qu\'est-ce qui nous permet de valider l\'architecture technique mise en place ?',
+    {texte: "Qu'est-ce qui nous permet de valider l'architecture technique mise en place ?",
       reponses: [
-        {id: 12, texte: 'Elle correspond à la demande et au contexte client.', correcte: false},
-        {id: 13, texte: 'Elle est fonctionnelle et éprouvée via un POC (proof of concept).', correcte: true},
-        {id: 14, texte: 'Elle est écrite exhaustivement sur un document de spécifications techniques.', correcte: false},
+        {id:12,texte:"Elle correspond à la demande et au contexte client.", correcte:false},
+        {id:13,texte:"Elle est fonctionnelle et éprouvée via un POC (proof of concept).", correcte:true},  
+        {id:14,texte:"Elle est écrite exhaustivement sur un document de spécifications techniques.", correcte:false},
       ]
     },
-    {texte: 'Qu\'est-ce qui nous permet de valider l\'architecture technique mise en place ?',
+    {texte: "Qu'est-ce qui nous permet de valider l'architecture technique mise en place ?",
       reponses: [
-        {id: 15, texte: 'Elle correspond à la demande et au contexte client.', correcte: false},
-        {id: 16, texte: 'Elle est fonctionnelle et éprouvée via un POC (proof of concept).', correcte: true},
-        {id: 17, texte: 'Elle est écrite exhaustivement sur un document de spécifications techniques.', correcte: false},
+        {id:15,texte:"Elle correspond à la demande et au contexte client.", correcte:false},
+        {id:16,texte:"Elle est fonctionnelle et éprouvée via un POC (proof of concept).", correcte:true},  
+        {id:17,texte:"Elle est écrite exhaustivement sur un document de spécifications techniques.", correcte:false},
       ]
     },
-    {texte: 'Le client vous appelle pour vous demander un petit changement qui n\'était pas prévu',
+    {texte: "Le client vous appelle pour vous demander un petit changement qui n'était pas prévu",
       reponses: [
-        {id: 18, texte: 'Pour conserver un bon contact avec le client, vous lui faite la modif.', correcte: false},
-        {id: 19, texte: 'Vous écoutez le client sans vous engager et vous demandez l\'avis du chef de projet.', correcte: true},
-        {id: 20, texte: 'Vous raccrochez directement.', correcte: false},
+        {id:18,texte:"Pour conserver un bon contact avec le client, vous lui faite la modif.", correcte:false},
+        {id:19,texte:"Vous écoutez le client sans vous engager et vous demandez l'avis du chef de projet.", correcte:true},  
+        {id:20,texte:"Vous raccrochez directement.", correcte:false},
       ]
     },
-    {texte: 'Qu\'est-ce qu\'une exigence ?',
+    {texte: "Qu'est-ce qu'une exigence ?",
       reponses: [
-        {id: 21, texte: 'La description du besoin du client .', correcte: true},
-        {id: 22, texte: 'Les règles d\'architecture de la solution informatique.', correcte: false},
-        {id: 23, texte: 'Le périmètre du projet.', correcte: false},
+        {id:21,texte:"La description du besoin du client .", correcte:true},
+        {id:22,texte:"Les règles d'architecture de la solution informatique.", correcte:false},  
+        {id:23,texte:"Le périmètre du projet.", correcte:false},
       ]
     }
   ];
@@ -78,78 +77,72 @@ export class AppComponent {
   public count;
   public count$;
   public showButton;
-  public iconsList = [
-  'umbrella', 'coffee', 'wrench',
-  'hand-spock', 'feather', 'futbol',
-  'fish', 'grin-tongue-wink', 'cut',
-  'code-branch', 'fire-extinguisher', 'key',
-  'heart', 'fire', 'paw',
-  'thermometer', 'shower', 'utensil-spoon',
-  'camera', 'music', 'rocket',
-  'trophy', 'truck', 'concierge-bell'];
+ // public idsList=[0,1,2,3,4,5,6,7,8,9];
+  //map
+  public iconsList=["coffee","wrench","umbrella",
+                    "hand-spock", "futbol", "feather",
+                    "fish", "grin-tongue-wink", "car-side"
+                  ,"cut","code-branch", "fire-extinguisher",'anchor'];
 
 
-  constructor() {
+  constructor(){
     this.shuffleQuestionsReponses();
     this.currentIndex = 0;
     this.showButton = true;
-    this.countStartValue = 1 * 10;
+    this.countStartValue = 1*10;
     this.count = new BehaviorSubject<number>(this.countStartValue);
     this.count$ = this.count.asObservable();
-    const subscription = this.count$.subscribe(c => {
-      if (c === 0) { this.end(); }
-    });
+    let subscription = this.count$.subscribe(c => {
+      if (c == 0) {this.end()};
+  });
 
  }
 
- begin() {
+ begin(){
   this.showButton = false;
-  this.showQuestions = true;
-  // this.goTimer();
+  this.showQuestions =true;
+  //this.goTimer();
 
  }
 
- end() {
-    console.log('end');
+ end(){
+    console.log("end");
     this.showQuestions = false;
  }
 
- openBox() {
-  console.log('openBox');
+ openBox(){
+  console.log("openBox");
   this.showQuestions = false;
 }
 
- increaseIndex() {
-  console.log( this.currentIndex + '/' + String(this.questions.length - 1));
-  this.currentIndex < this.questions.length - 1 ? this.currentIndex++ : this.openBox();
+ increaseIndex(){
+  console.log( this.currentIndex + "/" + String(this.questions.length-1)); 
+  this.currentIndex < this.questions.length-1 ? this.currentIndex++ : this.openBox();
 
 
 }
 
-counterNextValue() {
-  this.count.next(this.count.getValue() - 1);
+counterNextValue(){
+  this.count.next(this.count.getValue()-1);
   return this.count.getValue();
 }
 
-  goTimer() {
+  goTimer(){
     console.log('goTimer');
-    this.countDown =
-      timer(0, 1000)
+    this.countDown = 
+      timer(0,1000)
         .pipe(
         take(this.count.getValue()),
-        map(() => Math.floor(this.counterNextValue() / 60) + ':' + (this.count.getValue() % 60)
+        map(()=> Math.floor(this.counterNextValue() / 60) + ':' + (this.count.getValue() % 60)
         ));
   }
 
-  shuffleQuestionsReponses() {
+  shuffleQuestionsReponses(){
     this.questions = this.shuffle(this.questions);
     this.questions.forEach(question => {
       question.reponses = this.shuffle(question.reponses);
     });
-
-    console.log(this.iconsList);
     this.shuffle(this.iconsList);
-    console.log(this.iconsList);
   }
 
   shuffle(a) {
@@ -158,6 +151,10 @@ counterNextValue() {
         [a[i], a[j]] = [a[j], a[i]];
     }
     return a;
+  } 
+
+  unlock(key) {
+    console.log(key)
   }
 
 }
